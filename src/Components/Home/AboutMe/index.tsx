fad2656fb2cc78ae331ec9696f8ce772214a9249
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-09-26 19:11:27 -0300

    about-me content

--- src/Components/Home/AboutMe/index.tsx
+++ src/Components/Home/AboutMe/index.tsx
@@ -40,14 +40,14 @@ export function AboutMe() {
                             <div className="relative right-6 w-4 h-4 bg-gray-200 border-2 border-gray-950 rounded-full" />
                             <div className="relative bottom-5">
                                 <h2 className="font-semibold text-lg font-sans">Eu</h2>
-                                <p className="text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus aliquid deleniti quia sed dolorum consectetur repellendus eaque cupiditate delectus exercitationem sapiente at voluptas assumenda, sit, voluptatibus quod omnis voluptates odio.</p>
+                                <p className="text-sm">Meu nome é Pedro Paulino Martiniano, tenho 19 anos e sou um desenvolvedor backend em busca de uma nova oportunidade para mostrar meu conhecimento! Iníciei no mundo da programação como quase todo mundo, aprendendo HTML, CSS e  JavaScript, e fui progredindo para bibliotecas e frameworks como o ReactJs e NextJs, porém descobri que minha verdadeira paixão é o BackEnd, que é onde eu estou depositando todas minhas horas de estudo diário para me aprofundar cada dia mais nesse mundo!</p>
                             </div>
                         </div>
                         <div className="pl-4">
                             <div className="relative bottom-5">
                                 <div className="relative right-6 w-4 h-4 bg-gray-200 border-2 border-gray-950 rounded-full" />
                                 <h2 className="font-semibold text-lg font-sans">Desenvolvedor Backend</h2>
-                                <p className="text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus aliquid deleniti quia sed dolorum consectetur repellendus eaque cupiditate delectus exercitationem sapiente at voluptas assumenda, sit, voluptatibus quod omnis voluptates odio.</p>
+                                <p className="text-sm">Iniciei meus estudos aprofundados em backend no começo de 2023, com o NodeJs, e descobri que realmente amava fazer aquilo, tenho também o desejo de aprendender diferentes linguagens backend's. Possuo conhecimento prático em criação e manutenção de API's Rest utilizando NodeJs, juntamente com express, fastify e bibliotecas como JWT, zod, Prisma, KnexJs e MySQL2. Possuo conhecimento prévio em banco de dados MySQL, PostgreSQL e MongoDB.</p>
                             </div>
                         </div>
 
@@ -55,7 +55,7 @@ export function AboutMe() {
                             <div className="relative bottom-5">
                                 <div className="relative right-6 w-4 h-4 bg-gray-200 border-2 border-gray-950 rounded-full" />
                                 <h2 className="font-semibold text-lg font-sans">Estudante</h2>
-                                <p className="text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus aliquid deleniti quia sed dolorum consectetur repellendus eaque cupiditate delectus exercitationem sapiente at voluptas assumenda, sit, voluptatibus quod omnis voluptates odio.</p>
+                                <p className="text-sm">Atualmente estou cursando o 4º semestre de Ciência da Computação na Uni-Facef, onde iniciei meus estudos em fevereiro de 2022, possuo três certificações de reconhecimento pela melhor média da minha turma no semestre, e uma certificação de Database Design - Oracle, pelo êxito da disciplina de Banco de Dados I. Também sou apaixonado por aprender coisas novas e aprimorar minhas habilidades, e por isso, estou sempre em busca de cursos e estudos em áreas/assuntos que sei que são de grande importância para minha evolução pessoal, técnica e profissional.</p>
                             </div>
                         </div>
                     </div>
