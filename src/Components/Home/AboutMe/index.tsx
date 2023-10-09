093be2005200351c7bdb1426053937ca7909bc4e
PedroMartiniano <110617020+PedroMartiniano@users.noreply.github.com>
2023-10-09 02:07:53 -0300

    new texts

--- src/Components/Home/AboutMe/index.tsx
+++ src/Components/Home/AboutMe/index.tsx
@@ -41,7 +41,7 @@ export function AboutMe() {
                             <div className="relative right-6 w-4 h-4 bg-gray-200 border-2 border-gray-950 rounded-full" />
                             <div className="relative bottom-5">
                                 <h2 className="font-semibold text-lg font-sans">Eu</h2>
-                                <p className="text-sm">Meu nome é Pedro Paulino Martiniano, tenho 19 anos e sou um desenvolvedor backend em busca de uma nova oportunidade para demonstrar meu conhecimento! Iníciei no mundo da programação como quase todo mundo, aprendendo HTML, CSS e  JavaScript, e fui progredindo para bibliotecas e frameworks como o ReactJs e NextJs, porém descobri que minha verdadeira paixão é o BackEnd, onde eu estou depositando todas minhas horas de estudo diário para me aprofundar cada dia mais nesse mundo!</p>
+                                <p className="text-sm text-justify">Meu nome é Pedro Paulino Martiniano, tenho 19 anos e sou um desenvolvedor backend em busca de uma nova oportunidade para demonstrar meu conhecimento! Iníciei no mundo da programação como quase todo mundo, aprendendo HTML, CSS e  JavaScript, e fui progredindo para bibliotecas e frameworks como o ReactJs e NextJs, porém descobri que minha verdadeira paixão é o BackEnd, onde eu estou depositando todas minhas horas de estudo diário para me aprofundar cada dia mais nesse mundo!</p>
                             </div>
                         </div>
 
@@ -49,7 +49,7 @@ export function AboutMe() {
                             <div className="relative right-6 w-4 h-4 bg-gray-200 border-2 border-gray-950 rounded-full" />
                             <div className="relative bottom-5">
                                 <h2 className="font-semibold text-lg font-sans">Estudante</h2>
-                                <p className="text-sm">Atualmente estou cursando o 4º semestre de Ciência da Computação na Uni-Facef, onde iniciei meus estudos em fevereiro de 2022, possuo três certificações de reconhecimento pela melhor média da minha turma no semestre, e uma certificação de Database Design - Oracle, pelo êxito da disciplina de Banco de Dados I. Também sou apaixonado por aprender coisas novas e aprimorar minhas habilidades, e por isso, estou sempre em busca de cursos e estudos em áreas/assuntos que sei que são de grande importância para minha evolução pessoal, técnica e profissional.</p>
+                                <p className="text-sm text-justify">Atualmente estou cursando o 4º semestre de Ciência da Computação na Uni-Facef, onde iniciei meus estudos em fevereiro de 2022, possuo três certificações de reconhecimento pela melhor média da minha turma no semestre, e uma certificação de Database Design - Oracle, pelo êxito da disciplina de Banco de Dados I. Também sou apaixonado por aprender coisas novas e aprimorar minhas habilidades, e por isso, estou sempre em busca de cursos e estudos em áreas/assuntos que sei que são de grande importância para minha evolução pessoal, técnica e profissional.</p>
                             </div>
                         </div>
 
@@ -57,7 +57,23 @@ export function AboutMe() {
                             <div className="relative right-6 w-4 h-4 bg-gray-200 border-2 border-gray-950 rounded-full" />
                             <div className="relative bottom-5">
                                 <h2 className="font-semibold text-lg font-sans">Desenvolvedor Backend</h2>
-                                <p className="text-sm">Iniciei meus estudos aprofundados em backend no começo de 2023, com o NodeJs, e descobri que realmente amava fazer aquilo, tenho também o desejo de aprender diferentes linguagens backend. Possuo conhecimento prático em criação e manutenção de APIs Rest utilizando NodeJs, juntamente com express, fastify e bibliotecas como JWT, zod, Prisma, KnexJs e MySQL2. Possuo conhecimento prévio em banco de dados MySQL, PostgreSQL e MongoDB.</p>
+                                <p className="text-sm text-justify">Iniciei meus estudos aprofundados em backend no começo de 2023, com o NodeJs, e tenho também o desejo de aprender diferentes linguagens backend futuramente. Possuo conhecimento prático em criação e manutenção de APIs Rest utilizando NodeJs, juntamente com Express, Fastify e bibliotecas como JWT, Zod, Prisma, KnexJs, MySQL2 e conhecimento prévio em banco de dados MySQL, PostgreSQL e MongoDB. Também tenho experiência em designs patterns como o Controller-Service-Repository e Factory, utilizando POO para possível reutilização de código e inversão de dependência respectivamente!</p>
+                            </div>
+                        </div>
+
+                        <div className="pl-4">
+                            <div className="relative right-6 w-4 h-4 bg-gray-200 border-2 border-gray-950 rounded-full" />
+                            <div className="relative bottom-5">
+                                <h2 className="font-semibold text-lg font-sans">Experiência</h2>
+                                <div className="flex">
+                                    <div className=" mt-2 mr-2 w-2 h-2 rounded-full bg-black"></div>
+                                    <h3 className="font-medium text-[16px] font-sans">Estagiário desenvolvedor Backend - <span className="italic text-[14px]">ACT - Ciência, tecnologia e inovação.</span></h3>
+                                </div>
+                                <p className="my-1 text-sm ml-4 text-justify">Projeto de formação de desenvolvedores que tive o prazer de fazer parte durante todos os 3 meses de duração. Fui responsável por desenvolver e dar manutenção em APIs Rest utilizando o runtime NodeJs, TypeScript e Express, juntamente com estrátegias de otimização de código como clean code, e POO, também metodologias ágeis como Scrum e Kanban e realização de testes unitários e testes de integração. Porém também tive contato com a criação de aplicações web utilizando ReactJs e NextJs, assim como o portfólio que você está lendo nesse momento!</p>
+                                <div className="flex">
+                                    <div className="mt-2 mr-2 w-2 h-2 rounded-full bg-black"></div>
+                                    <h3 className="font-medium text-[16px] font-sans">Em busca de uma nova oportunidade...</h3>
+                                </div>
                             </div>
                         </div>
 
--- src/Components/Home/LandingPage/index.tsx
+++ src/Components/Home/LandingPage/index.tsx
@@ -15,11 +15,11 @@ export function LandingPage() {
             <div className="flex flex-col gap-6">
                 <h1 className="font-sans font-extrabold text-3xl self-start">Olá, meu nome é <span className="text-yellow-600">Pedro Paulino Martiniano</span></h1>
                 <h2 className="font-sans font-extrabold text-2xl self-start text-yellow-600">{`{ `}Desenvolvedor BackEnd{` }`}</h2>
-                <p className="text-sm">
+                <p className="text-sm text-justify">
                     Seja muito bem vindo ao meu portfólio! Aqui você descobrirá um pouco sobre minhas habilidades, tecnologias que trabalho e projetos feitos por mim até o momento.
                 </p>
-                <p className="text-sm">
-                    Sou uma pessoa que busca estar em constante evolução e que é apaixado por tecnologia! Minha missão é desenvolver softwares seguros e eficientes, que sejam de grande utilidade para a sociedade.
+                <p className="text-sm text-justify">
+                    Sou uma pessoa que busca estar em constante evolução e que é apaixado por tecnologia! Minha missão é desenvolver softwares seguros, eficientes e escaláveis, que sejam de grande utilidade para a sociedade.
                 </p>
                 <div className="flex flex-col gap-6 lg:flex-row">
                     <Button variant="outline" onClick={() => router.push('/#about-me')}>Sobre Mim</Button>
