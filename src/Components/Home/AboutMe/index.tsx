c5b711b35ba7f5b849e17fc1b7f4101ed3e56c08
pedro <pedropmartiniano@gmail.com>
2025-03-09 21:18:33 -0300

    updating picture and adding project

--- src/Components/Home/AboutMe/index.tsx
+++ src/Components/Home/AboutMe/index.tsx
@@ -13,7 +13,7 @@ export function AboutMe() {
             <h1 className="font-bold font-sans text-2xl lg:text-3xl mb-8 lg:ml-10">{'{ '}Sobre mim{' }'}</h1>
             <div className="flex flex-col gap-6 lg:grid lg:grid-cols-2 justify-start items-start min-h-screen p-5">
                 <div className="flex flex-col w-full justify-center items-center">
-                    <img src="/foto-perfil.jpeg" alt="foto-perfil" className="rounded-full w-48 h-48 lg:w-72 lg:h-72 shadow-xl" />
+                    <img src="/foto-perfil-ccm.jpg" alt="foto-perfil" className="rounded-full w-48 h-48 lg:w-72 lg:h-72 shadow-xl" />
                     <div className="flex w-full justify-center items-center gap-1 mt-5">
                         <Link href={'https://www.linkedin.com/in/pedro-paulino-martiniano/'} passHref={true} target="_blanck">
                             <Button variant="ghost" className="w-13 h-13 cursor-pointer rounded-full">
@@ -41,7 +41,7 @@ export function AboutMe() {
                             <div className="relative right-6 w-4 h-4 bg-gray-200 border-2 border-gray-950 rounded-full" />
                             <div className="relative bottom-5">
                                 <h2 className="font-semibold text-lg font-sans">Eu</h2>
-                                <p className="text-sm text-justify">Meu nome é Pedro Paulino Martiniano, tenho 19 anos e sou um desenvolvedor back-end! Iníciei no mundo da programação como quase todo mundo, aprendendo HTML, CSS e  JavaScript, e fui progredindo para bibliotecas e frameworks como o ReactJs e NextJs, porém descobri que minha verdadeira paixão é o BackEnd, onde eu estou depositando todas minhas horas de estudo diário para me aprofundar cada dia mais nesse mundo!</p>
+                                <p className="text-sm text-justify">Meu nome é Pedro Paulino Martiniano, tenho 20 anos e sou um desenvolvedor back-end! Iníciei no mundo da programação como quase todo mundo, aprendendo HTML, CSS e  JavaScript, e fui progredindo para bibliotecas e frameworks como o ReactJs e NextJs, porém descobri que minha verdadeira paixão é o BackEnd, onde eu estou depositando todas minhas horas de estudo diário para me aprofundar cada dia mais nesse mundo!</p>
                             </div>
                         </div>
 
@@ -49,7 +49,7 @@ export function AboutMe() {
                             <div className="relative right-6 w-4 h-4 bg-gray-200 border-2 border-gray-950 rounded-full" />
                             <div className="relative bottom-5">
                                 <h2 className="font-semibold text-lg font-sans">Estudante</h2>
-                                <p className="text-sm text-justify">Atualmente estou cursando o 6º semestre de Engenharia de Software na Uni-FACEF, onde iniciei meus estudos em fevereiro de 2022, com previsão de término em 2025, possuo quatro certificações de reconhecimento pela melhor média da minha turma no semestre, e três certificações Banco de Dados Oracle. Também sou apaixonado por aprender coisas novas e aprimorar minhas habilidades, e por isso, estou sempre em busca de cursos e estudos em áreas/assuntos que sei que são de grande importância para minha evolução pessoal, técnica e profissional.</p>
+                                <p className="text-sm text-justify">Atualmente estou cursando o 7º semestre de Engenharia de Software na Uni-FACEF, onde iniciei meus estudos em fevereiro de 2022, com previsão de término em 2025, possuo quatro certificações de reconhecimento pela melhor média da minha turma no semestre, e três certificações Banco de Dados Oracle. Também sou apaixonado por aprender coisas novas e aprimorar minhas habilidades, e por isso, estou sempre em busca de cursos e estudos em áreas/assuntos que sei que são de grande importância para minha evolução pessoal, técnica e profissional.</p>
                             </div>
                         </div>
 
@@ -57,7 +57,7 @@ export function AboutMe() {
                             <div className="relative right-6 w-4 h-4 bg-gray-200 border-2 border-gray-950 rounded-full" />
                             <div className="relative bottom-5">
                                 <h2 className="font-semibold text-lg font-sans">Desenvolvedor Backend</h2>
-                                <p className="text-sm text-justify">Iniciei meus estudos aprofundados em backend no começo de 2023, com o NodeJs, desde lá, os caminhos que segui me levaram a aprender Golang, linguagem na qual atuo atualmente. Possuo conhecimento prático em criação e manutenção de APIs Restful seguras e escaláveis em ambientes de microsserviços, buscando sempre as tecnologias mais atuais para me auxiliar nesse processo. Possuo conhecimento prévio em banco de dados SQL e NoSQL. Experiência também em designs patterns, principios SOLID, Arqutetura Hexagonal e POO.</p>
+                                <p className="text-sm text-justify">Iniciei meus estudos aprofundados em backend no começo do ano de 2023, com o NodeJs, desde lá, os caminhos que segui me levaram a aprender Golang e Python, linguagens na qual atuo atualmente. Possuo conhecimento prático em criação e manutenção de APIs Restful seguras e escaláveis em ambientes de microsserviços, buscando sempre as tecnologias mais atuais para me auxiliar nesse processo. Possuo conhecimento prévio em banco de dados SQL e NoSQL. Experiência também em designs patterns, principios SOLID, Arqutetura Hexagonal, DDD e POO.</p>
                             </div>
                         </div>
 
@@ -68,19 +68,19 @@ export function AboutMe() {
 
                                 <div className="flex bg-[#c3c4c8] p-[2px] rounded-xl">
                                     <div className="mt-2 mr-2 ml-1 w-2 h-2 rounded-full bg-black"></div>
-                                    <h3 className="font-medium text-[15px] font-sans">Desenvolvedor Back-End Júnior - <span className="italic text-[14px]">CCM Tecnologia</span></h3>
+                                    <h3 className="font-medium text-[15px] font-sans">Desenvolvedor Back-End Júnior - <span className="italic text-[14px]">CCM Tecnologia</span> | Agosto 2024 - Atualmente</h3>
                                 </div>
                                 <p className="my-1 text-sm ml-4 text-justify">Atuando no time de P&D da CCM Tecnologia, sou responsável pelo desenvolvimento e manutenção de APIs Rest e Orientadas a Eventos de alto nível em ambiente de microsserviços utilizando Golang e Node.js, trabalhando também com serviços de mensageria e plataformas Cloud como AWS e OCI, sempre visando escalabilidade, segurança, e performance, utilizando metodologias como clean code, SOLID principles, Hexagonal Architecture e Scrum. Também fiz parte de projetos de integração de dados sendo responsável por criação de scripts para execução em Cron Jobs utilizando Python e Pandas.</p>
 
                                 <div className="flex bg-[#c3c4c8] p-[2px] rounded-xl">
                                     <div className="mt-2 mr-2 ml-1 w-2 h-2 rounded-full bg-black"></div>
-                                    <h3 className="font-medium text-[15px] font-sans">Estágio em desenvolvimento back-end - <span className="italic text-[14px]">CCM Tecnologia</span></h3>
+                                    <h3 className="font-medium text-[15px] font-sans">Estágio em desenvolvimento back-end - <span className="italic text-[14px]">CCM Tecnologia</span> | Dezembro 2023 - Agosto 2024</h3>
                                 </div>
                                 <p className="my-1 text-sm ml-4 text-justify">Durante meu estágio no time de P&D da CCM Tecnologia, participei no suporte ao desenvolvimento e manutenção de APIs Rest e microsserviços, utilizando Golang e Node.js. Contribuí na implementação de serviços de mensageria e trabalhei com plataformas Cloud como AWS e OCI. Apoiei projetos de integração de dados, criando scripts em Python para execução em Cron Jobs, sempre colaborando com a equipe para aplicar boas práticas como clean code e SOLID principles.</p>
 
                                 <div className="flex bg-[#c3c4c8] p-[2px] rounded-xl">
                                     <div className="mt-2 mr-2 ml-1 w-2 h-2 rounded-full bg-black"></div>
-                                    <h3 className="font-medium text-[15px] font-sans">Estágio em desenvolvimento back-end - <span className="italic text-[14px]">ACT - Ciência, tecnologia e inovação</span></h3>
+                                    <h3 className="font-medium text-[15px] font-sans">Estágio em desenvolvimento back-end - <span className="italic text-[14px]">ACT - Ciência, tecnologia e inovação</span> | Julho 2023 - Setembro 2023</h3>
                                 </div>
                                 <p className="my-1 text-sm ml-4 text-justify">Projeto de formação de desenvolvedores que tive o prazer de fazer parte durante todos os 3 meses de duração. Fui responsável por desenvolver e dar manutenção em APIs Rest utilizando NodeJs, TypeScript e Express, juntamente com estrátegias de otimização de código como clean code, e POO, também metodologias ágeis como Scrum e Kanban e realização de testes unitários e testes de integração. Porém também tive contato com a criação de aplicações web front-end utilizando ReactJs e NextJs, assim como o portfólio que você está lendo nesse momento!</p>
 
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,6 +8,22 @@ export default function Projects() {
 
             <div className="flex flex-col gap-6 lg:grid lg:grid-cols-2 justify-start items-start p-5 border-b-2 pt-20">
                 <div className="flex flex-col w-full justify-center items-center">
+                    <img src="/golang.png" alt="foto-projeto" className="p-5 border-2 border-gray-950 rounded-md shadow-inner hover:brightness-90" width="500" height="400"/>
+                </div>
+
+                <div className="flex flex-col justify-center items-start gap-6 lg:gap-6">
+                    <h2 className="text-xl font-sans font-bold text-yellow-600">E-commerce Microsserviços</h2>
+                    <p className="text-md lg:text-md text-justify">Microsserviços construidos utilizando a linguagem Go, com o framework Gin Gonic, para o desenvolvimento de APIs Rest, e para o banco de dados foi utilizado o PostgreSQL. Para o gerenciamento de filas foi utilizado o RabbitMQ e para o gerenciamento do projeto foi utilizado o Docker e Docker Compose. As APIs foram construídas utilizando o padrão de arquitetura hexagonal, SOLID principles, DDD e Clean Architecture.</p>
+                    <h3 className="text-md font-sans font-semibold">Tecnologias: Golang, Gin Gonic, Docker, Docker Compose, PostgreSQL, RabbitMQ, entre outras!</h3>
+                    <Button variant="outline" className="rounded-lg w-80">
+                        <a href="https://github.com/PedroMartiniano/ecommerce" target="_blank">Acessar este projeto no GitHub</a>
+                    </Button>
+                </div>
+            </div>
+            <div className="border-b-2 border-gray-800 mx-10 lg:mx-20 w-auto" />
+
+            <div className="flex flex-col gap-6 lg:grid lg:grid-cols-2 justify-start items-start p-5 border-b-2 pt-20">
+                <div className="flex flex-col w-full justify-center items-center">
                     <img src="/estruturas-de-dados.jpg" alt="foto-projeto" className="p-5 border-2 border-gray-950 rounded-md shadow-inner hover:brightness-90" width="500" height="400"/>
                 </div>
 
